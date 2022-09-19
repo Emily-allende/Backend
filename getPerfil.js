889d74786ee0c9de70a4c04141b93d2d868a5e81
Emily-allende <79849579+Emily-allende@users.noreply.github.com>
2022-09-19 18:10:58 -0300

    new commit

--- getPerfil.js
+++ getPerfil.js
@@ -3,8 +3,11 @@ const { PrismaClient } = require('@prisma/client')
 
 const app = express()
 const prisma = new PrismaClient()
+app.use(express.json())
 
 app.delete('/usuario', async (req, res)=>{
-    const posts = await prisma.user.findMany({})
+    const posts = await prisma.usuario.findMany({})
     res.json(posts)
 })
+
+app.listen(3000, ()=> console.log("Estoy corriendo"))
