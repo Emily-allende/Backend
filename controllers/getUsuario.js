06248e96614a7d16c85abbb4faf95cc1b4c7b94b
Emily-allende <emilyallende03@gmail.com>
2022-10-14 10:01:03 -0300

    commit para guardar

--- controllers/getUsuario.js
+++ controllers/getUsuario.js
@@ -1,16 +1,13 @@
 import express from 'express';
 import { PrismaClient } from '@prisma/client';
-// import { createRequire } from 'module';
 
 const prisma = new PrismaClient()
 const usuRouter = express.Router();
 
-usuRouter.get("/:UserID", async ({params: {UserID}}, res) => { 
-  // const require = createRequire(import.meta.url);
-  const post = await prisma.usuario.findUnique({where: {id: UserID}})
-  res.json(post);
-  // console.log(post);
-  // const url = require('url').parse(req.url, true).query;
+usuRouter.get("/", async (req, res) => { 
+  console.log("hola");
+  const post = await prisma.usuario.findUnique({where: {ID: 222}})
+  console.log(post);
 })
 
 export {usuRouter};
--- controllers/postUsuario.js
+++ controllers/postUsuario.js
@@ -5,12 +5,11 @@ const prisma = new PrismaClient()
 const usuarioRouter = express.Router();
 usuarioRouter.post("/", (req, res) => { 
   console.log("Llege")
-  const { nombre, apellido, empresa, email, contraseña, img_perfil, ubicacion:{lat, long}} = req.body
+  const { nombre, apellido, email, contraseña, img_perfil, ubicacion:{lat, long}} = req.body
   const post =  prisma.usuario.create({
     data: {
       nombre,
       apellido,
-      empresa,
       email, 
       contraseña,
       img_perfil,
@@ -22,45 +21,6 @@ usuarioRouter.post("/", (req, res) => {
   })
   res.json(post);
   console.log(post);
-  // return jwt.sign({post}, "secretkey", (err, token)=> {
-  //   res.json(token);
-  // })
 })
 
 export {usuarioRouter};
-
-// import { Transport } from "./../confing/mail";
-// const jwt = require("jsonwebtoken")
-// app.post( async (req, res)=>{
-//nombre, apellido, empresa, email, contraseña, img_perfil, ubicacion
-//     const user = await prisma.usuario.create({
-//       data:{
-//         nombre, 
-//         apellido, 
-//         empresa, 
-//         email, 
-//         contraseña, 
-//         img_perfil, 
-//         ubicacion:{
-//           lat, 
-//           long
-//         }},
-    //     return: jwt.sign({user},"secretkey", (err, token)=> {
-    //       res.json({token})
-    //     })
-    // });
-  // });
-// try{
-//   await transporter.sendMail({
-//     from: '"EcoLoops" <foo@example.com>', 
-//     to: email, 
-//     subject: "EcoLoops", 
-//     html: `
-//     <b>Hola ${nombre}, Bienvenida a la comunidad EcoLoops</b>
-//     <b>Muchas gracias</b>
-//     `,
-//   });
-// }catch(error){
-//   return res.status(400).json({message:'Error'})
-
-
